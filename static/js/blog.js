d9bab8a2f3bc0e146c9e17b6640dbc26d61a7946
Abhishek Pednekar <abhi_ap@hotmail.com>
2019-11-12 16:45:45 +0530

    Modifications to the blog page

--- static/js/blog.js
+++ static/js/blog.js
@@ -10,3 +10,19 @@ navItemsBlog.forEach(itemBlog => itemBlog.classList.remove('current'));
 
 // Add the current class for the portfolio list item
 liCurrent.classList.add('current');
+
+$(document).ready(function () {
+    // This hack is needed since the list of blogs is being generated via for loop
+    $("li").each(function (index) {
+        var index = index + 1; // increment the index since it starts at 0
+        var list = "li.blog-list" + index; // append the index to the class name since                                         they are generated fynamically in jinja2 
+        var box = "#box" + index;
+        $(box).hide();
+        $(box).addClass("blog-description")
+
+        $(list).click(function () {
+            $(box).slideToggle()
+        })
+    })
+
+})
